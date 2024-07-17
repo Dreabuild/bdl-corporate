0177c6e5c9ab68dd748ec8d2c46c7c795074d669
Saidur Rahman <saidurhere@gmail.com>
2024-07-17 21:18:25 +0600

    All done

--- src/components/PhotoGallery.jsx
+++ src/components/PhotoGallery.jsx
@@ -1,3 +1,9 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
+import { Autoplay } from "swiper/modules";
+
 const PhotoGallery = () => {
   const helpItems = [
     {
@@ -7,7 +13,37 @@ const PhotoGallery = () => {
     },
     {
       title: "কমিক বই",
-      image: " https://i.ibb.co/6YnCWK7/image-2.png",
+      image: "https://i.ibb.co/6YnCWK7/image-2.png",
+      link: "#",
+    },
+    {
+      title: "দিনব্যাপী কর্মশালা",
+      image: "https://i.ibb.co/Yj2bTFK/image-3.png",
+      link: "#",
+    },
+    {
+      title: "ভিডিও সিরিজ শুটিং",
+      image: "https://i.ibb.co/xCPGRJg/image-1.png",
+      link: "#",
+    },
+    {
+      title: "কমিক বই",
+      image: "https://i.ibb.co/6YnCWK7/image-2.png",
+      link: "#",
+    },
+    {
+      title: "দিনব্যাপী কর্মশালা",
+      image: "https://i.ibb.co/Yj2bTFK/image-3.png",
+      link: "#",
+    },
+    {
+      title: "ভিডিও সিরিজ শুটিং",
+      image: "https://i.ibb.co/xCPGRJg/image-1.png",
+      link: "#",
+    },
+    {
+      title: "কমিক বই",
+      image: "https://i.ibb.co/6YnCWK7/image-2.png",
       link: "#",
     },
     {
@@ -27,32 +63,57 @@ const PhotoGallery = () => {
           data-aos-delay={300}
           data-aos-duration="500"
         >
-          ফটো <span className=" text-[#BF0000]"> গ্যালারী</span>
+          ফটো <span className="text-[#BF0000]">গ্যালারী</span>
         </h2>
-        <div className="grid lg:md:grid-cols-3 grid-cols-1 gap-6">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={30}
+          loop={true}
+          loopAdditionalSlides={true}
+          longSwipes={true}
+          autoplay={{
+            delay: 500,
+            disableOnInteraction: true,
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 1,
+            },
+            768: {
+              slidesPerView: 2,
+            },
+            1024: {
+              slidesPerView: 3,
+            },
+          }}
+          modules={[Autoplay]}
+          className="mySwiper"
+        >
           {helpItems.map((item, index) => (
-            <div
+            <SwiperSlide
               key={index}
-              className={`rounded-3xl flex cursor-pointer items-center justify-end`}
+              className={`rounded-3xl flex cursor-pointer items-center justify-end ${
+                index === 0 ? "col-span-2" : "col-span-1"
+              }`}
               style={{
                 backgroundImage: `url(${item.image})`,
                 height: "240px",
                 backgroundRepeat: "no-repeat",
                 backgroundSize: "cover",
               }}
-              data-aos="fade-up"
-              data-aos-anchor-placement="center-bottom"
-              data-aos-delay={400 + index * 200}
-              data-aos-duration="500"
             >
-              <div className="w-1/2 h-full bg-gray-gradient flex items-center justify-center rounded-3xl">
+              <div
+                className={`w-${
+                  index === 0 ? "full" : "1/2"
+                } h-full bg-gray-gradient flex items-center justify-center rounded-3xl`}
+              >
                 <h3 className="text-white text-3xl font-semibold w-[60px]">
                   {item.title}
                 </h3>
               </div>
-            </div>
+            </SwiperSlide>
           ))}
-        </div>
+        </Swiper>
       </div>
     </div>
   );
